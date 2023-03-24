8dd8dc57a7e26593997913e2de2b2d37b6d0eac2
shaad82663 <shaad82663@gmail.com>
2023-03-24 12:13:35 +0530

    Add day-1 files

---
+++ day-1/execise-1/datatypes.js
@@ -0,0 +1,8 @@
+// Create datatypes.js file and use the JavaScript typeof operator 
+// to check different data types. Check the data type of each variable
+
+console.log(typeof 'Shadab') // string
+console.log(typeof 10) // number
+console.log(typeof false) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
---
+++ day-1/execise-1/variable.js
@@ -0,0 +1,39 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+
+/* comments can make code,
+      readable, 
+      easy to reuse and 
+      informative       */
+
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let s = "mystring"
+let b = true
+let u = undefined
+let n = null
+
+
+//Declare four variables without assigning values
+let p,q,r,t
+
+let w = 0
+let x = 1
+let y = 2
+let z = 3
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+var firstName = "Mohd"
+var lastName = "Shadab"
+var maritalStatus = "Single"
+var country = "India"
+var age = 23
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName="Mohd", lastName="Shadab", maritalStatus="Single", country="India", age=23
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let yourAge = 30
+let myAge = 25
+
+console.log(`I am ${myAge} years old`)
+console.log(`You are ${yourAge} years old`)
