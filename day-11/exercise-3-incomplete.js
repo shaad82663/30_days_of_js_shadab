a5375f9fbed2e35c4ebd97c2542d767bcd92589b
shaad82663 <shaad82663@gmail.com>
2023-04-02 11:27:28 +0530

    edited day-11 files

--- day-11/exercise-3-incomplete.js
+++ day-11/exercise-3-incomplete.js
@@ -27,3 +27,41 @@ const students = [
 
 console.log(convertArrayToObject(students))
 
+
+// Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+// Add Bootstrap with level 8 to the front end skill sets
+// Add Express with level 9 to the back end skill sets
+// Add SQL with level 8 to the data base skill sets
+// Add SQL without level to the data science skill sets
+
+const addSkills = function(){
+  const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+      frontEnd: [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 8 },
+        { skill: 'React', level: 9 }
+      ],
+      backEnd: [
+        { skill: 'Node',level: 7 },
+        { skill: 'GraphQL', level: 8 },
+      ],
+      dataBase:[
+        { skill: 'MongoDB', level: 7.5 },
+      ],
+      dataScience:['Python', 'R', 'D3.js']
+    }
+  }
+    const studentCopy = {...student}
+    const {skills} = studentCopy
+    const {frontEnd, backEnd, dataBase, dataScience} = skills
+    frontEnd.push({skill : 'Bootstrap', level : 8})
+    backEnd.push({skill : 'Express', level : 9})
+    dataBase.push({skill : 'SQL', level : 8})
+    dataScience.push({skill : 'SQL', level : 8})
+    console.log(studentCopy.skills)
+}
+addSkills()
