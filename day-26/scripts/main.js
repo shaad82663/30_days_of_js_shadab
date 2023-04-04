af46ba87f53214659a15839c61fff066c0a5cc63
shaad82663 <shaad82663@gmail.com>
2023-04-04 18:04:35 +0530

    Added day-26 on 4th April

---
+++ day-26/scripts/main.js
@@ -0,0 +1,72 @@
+var searchFlag = 0 // Search flag to determine if the search is 'start with' -> (1) or 'any word' -> (2) 
+//To clear already renderd elements in search result
+const clearScreen = () => {
+   const resultItems = document.querySelector('.result-container')
+    resultItems.innerHTML = ''
+}
+
+const searchByStartingWord = (search) => {
+   clearScreen();
+   const result = countries.filter( country => country.toUpperCase().startsWith(search))
+   return result
+}
+
+const searchByContaingWords = (search) => {
+   clearScreen();
+    const result = countries.filter( country => country.toUpperCase().includes(search))
+    return result
+ }
+
+//  console.log(searchByStartingWord('A'))
+
+const startWordBtn = document.querySelector('.start-btn')
+const containBtn = document.querySelector('.contain-btn')
+const searchBtn = document.querySelector('#search-button')
+const searchBar = document.querySelector('input')
+const searchResultCount = document.querySelector('#search-result')
+const resultContainer = document.querySelector('.result-container')
+
+startWordBtn.addEventListener('click', () => {
+   startWordBtn.style.backgroundColor = '#15234b'
+   containBtn.style.backgroundColor = '#6c81bb'
+   searchBar.value = ''
+   clearScreen()
+    searchFlag = 1
+})
+containBtn.addEventListener('click', () => {
+   startWordBtn.style.backgroundColor = '#6c81bb'
+   containBtn.style.backgroundColor = '#15234b'
+   searchBar.value = ''
+   clearScreen()
+   searchFlag = 2
+})
+
+searchBar.addEventListener('input', (e) => {
+   var searchResult = []
+   var search = e.target.value
+   if(searchFlag == 1){
+       searchResult = searchByStartingWord(search.toUpperCase())
+       searchResultCount.textContent = `Countries starts with ${search} are ${searchResult.length}`
+      }else if(searchFlag == 2){
+         searchResult = searchByContaingWords(search.toUpperCase())
+       searchResultCount.textContent = `Countries contains ${search} are ${searchResult.length}`
+      }else{
+         clearScreen()
+         const warning = document.createElement('h1')
+         warning.textContent = 'Click on any one search type. Search with starting word or Search with any word!! '
+         warning.style.color='red'
+         warning.style.fontSize='30px'
+         warning.style.letterSpacing = '0'
+         warning.style.wordSpacing = '0'
+         warning.style.textAlign = 'center'
+         resultContainer.appendChild(warning)
+       }
+     
+      for(const country of searchResult){
+          const resultItem = document.createElement('div')
+          resultItem.className = 'result-item'
+          resultItem.textContent = country
+          resultContainer.appendChild(resultItem) 
+      }
+
+})
