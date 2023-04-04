22beb46d8ab46208b862d2bbd55350394b0f2db3
shaad82663 <shaad82663@gmail.com>
2023-04-04 11:47:57 +0530

    Added day-25 on 4th April

--- day-25/scripts/main.js
+++ day-25/scripts/main.js
@@ -2,44 +2,44 @@ const subtitle = document.querySelector('.subtitle')
 subtitle.textContent = `Currently we have ${countries_data.length} countries.`
 const allLanguages = []
 const allNationsPopulation = []
-for(const country of countries_data){
-   const {name ,languages, population} = country
-   allLanguages.push(languages)
-   allNationsPopulation.push({name : name, value : population})
+for (const country of countries_data) {
+    const { name, languages, population } = country
+    allLanguages.push(languages)
+    allNationsPopulation.push({ name: name, value: population })
 }
 
-allNationsPopulation.sort( (a,b) => b.value - a.value)
-const tenLargestCountries = allNationsPopulation.slice(0,10)
+allNationsPopulation.sort((a, b) => b.value - a.value)
+const tenLargestCountries = allNationsPopulation.slice(0, 10)
 var total = 0
-for(const country of allNationsPopulation){
-     total += Number(country.value)
+for (const country of allNationsPopulation) {
+    total += Number(country.value)
 }
 tenLargestCountries.unshift({
-    name : 'World',
-    value : total
+    name: 'World',
+    value: total
 })
 // console.log(tenLargestCountries)
 
 const countriesMap = new Map()
-for(const languages of allLanguages){
-    for(const language of languages){
-   if(!countriesMap.has(language))  countriesMap.set(language, 1)
-   else {
-      const freq = Number(countriesMap.get(language))
-      countriesMap.set(language, freq + 1)
-   }
+for (const languages of allLanguages) {
+    for (const language of languages) {
+        if (!countriesMap.has(language)) countriesMap.set(language, 1)
+        else {
+            const freq = Number(countriesMap.get(language))
+            countriesMap.set(language, freq + 1)
+        }
     }
 }
 // Define custom comparison function for sorting Map by value
-const mapSortByValue = (a, b) => b[1]-a[1]
+const mapSortByValue = (a, b) => b[1] - a[1]
 // Convert Map object to array and sort by value
-const tenMostSpokenLanguages = new Map([...countriesMap.entries()].sort(mapSortByValue).slice(0,10));
+const tenMostSpokenLanguages = new Map([...countriesMap.entries()].sort(mapSortByValue).slice(0, 10));
 const arr = Array.from(tenMostSpokenLanguages)  //converted to array
 const topTenLang = []
-for(const el of arr) {  // adding entries as object name -> lang name, value -> lang count
+for (const el of arr) {  // adding entries as object name -> lang name, value -> lang count
     topTenLang.push({
-        name : el[0],
-        value : el[1]
+        name: el[0],
+        value: el[1]
     })
 }
 //function for drawing chart
@@ -50,11 +50,11 @@ const drawChart = (data, title) => {
 
     //removing already rendered data (if any)
     const wrappers = document.querySelectorAll('.graph-wrapper')
-    if(wrappers.length){
-        for(const wrapper of wrappers)  document.body.removeChild(wrapper)
-    }  
+    if (wrappers.length) {
+        for (const wrapper of wrappers) document.body.removeChild(wrapper)
+    }
 
-    for(let i=0; i<data.length; i++){
+    for (let i = 0; i < data.length; i++) {
         const wrapper = document.createElement('div')
         wrapper.className = 'graph-wrapper'
         wrapper.id = 'stat'
@@ -64,7 +64,7 @@ const drawChart = (data, title) => {
         dataValue.textContent = data[i].value
         const graphDiv = document.createElement('div')
         // graphDiv.style.backgroundColor =  'red'
-        const widthPercentage = ((data[i].value * 100)/baseValue) 
+        const widthPercentage = ((data[i].value * 100) / baseValue)
         graphDiv.style.width = `${widthPercentage}%`
         graphDiv.style.height = '30px'
         wrapper.appendChild(dataName)
@@ -72,13 +72,13 @@ const drawChart = (data, title) => {
         wrapper.appendChild(dataValue)
         document.body.appendChild(wrapper)
     }
-}  
+}
 const buttons = document.querySelectorAll('button')
 
-buttons[0].addEventListener('click' , () => {
+buttons[0].addEventListener('click', () => {
     drawChart(topTenLang, '10 Most Spoken Language In The World')
 })
 
-buttons[1].addEventListener('click' , () => {
+buttons[1].addEventListener('click', () => {
     drawChart(tenLargestCountries, '10 Most Populated Countries In The World')
 })
